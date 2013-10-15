e828fdb6f576055902304db74205915bbfceecdc
fwehrle <franck.wehrle@gmail.com>
2013-10-15 22:49:14 +0200

    Update arduino.js
    Ajout de phrases d'erreurs et d'un time out

--- arduino.js
+++ arduino.js
@@ -9,18 +9,27 @@ exports.action = function(data, callback, config, SARAH) {
   if (act == 'get') {
   	adresse += 'sensor=' + data.sensor;
   }
+  var errTts = "L'action a échoué";
   if (act == 'set') {
   	adresse += data.actor + '=' + data.value;
+	if(data.value == '1'){
+		errTts = "Je n'ai pas pu allumer la lumière.";
+	}else{
+		errTts = "Je n'ai pas pu éteindre la lumière.";
+	}
   }
   
   console.log("adresse: " + adresse);
 
   var request = require('request');
-  request({ 'uri' : adresse }, function (err, response, body){
-    if (err || response.statusCode != 200) {
-      callback({'tts': "L'action a échoué"});
+  request({ 'uri' : adresse , 'timeout' : 5000}, function (err, response, body){
+  //request({ 'uri' : adresse }, function (err, response, body){
+  if (err || response.statusCode != 200) {
+      callback({'tts': errTts});
       return;
     }
-    callback({'tts' : body});
+
+    //callback({'tts' : body});
+	callback({});
   });
 }
